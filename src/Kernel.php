60d06feb1b29e8d13f8b9bc2f83dcdbf91f00e86
mabarroso <123025+mabarroso@users.noreply.github.com>
2021-01-18 14:10:53 +0100

    Package data

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Saltes\Data\Import;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
